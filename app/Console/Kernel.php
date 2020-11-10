205c3c69525cedb94fe8b0a8a20dd9162726311a
Evangelos Giannakosian <vgiannakosian@pavla.gr>
2020-11-10 10:42:09 +0200

    added laravel/telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
+        $schedule->command('telescope:prune --hours=48')->daily();
     }
 
     /**
